6b1b2fdd72ccf742277f6f5e8d907fe1884cb46e
seulgi <guseul9405@gmail.com>
2023-06-08 15:24:32 +0900

    집 그리기

--- app.js
+++ app.js
@@ -32,3 +32,31 @@ const ctx = canvas.getContext("2d"); // 그림을 그릴 수 있는 준비
 // ctx.stroke();
 // ctx.fill();
 
+// 집 그리기
+// ctx.moveTo(150, 50);
+// ctx.lineTo(75, 100);
+// ctx.lineTo(225, 100);
+// ctx.lineTo(150, 50);
+
+// ctx.moveTo(100, 100);
+// ctx.lineTo(100, 180);
+// ctx.lineTo(200, 180);
+// ctx.lineTo(200, 100);
+
+// ctx.stroke();
+////
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 5;
+ctx.strokeRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 30);
+
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.fill();
+
+ctx.fillRect(100, 400, 450, 5);
+
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(200, 200, 15, 100);
